0355d6370a34f3195b4b4bf6b84f14226302d8ac
root <tusharkanti.rauth@techprostudio.com>
2018-01-11 19:49:45 +0530

    tkr-11 jna

--- routes/things.js
+++ routes/things.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var express = require('express');
 var router = express.Router();
 
@@ -14,21 +13,4 @@ router.post('/', function(req, res){
 });
 
 //export this router to use in our index.js
-=======
-var express = require('express');
-var router = express.Router();
-
-router.get('/', function(req, res){
-   res.send('GET route on things.');
-});
-
-router.get('/hello', function(req, res){
-   res.send('test get result.');
-})
-router.post('/', function(req, res){
-   res.send('POST route on things.');
-});
-
-//export this router to use in our index.js
->>>>>>> 19e170ff9077989697cca98323f6ac5fda057642
 module.exports = router;
